5bca13be3b26ac37c7aea5b1ae6c67643da9dcd9
Fredrik Stockman <fredrikstockman@gmail.com>
2022-11-25 00:16:34 +0100

    Show todos in an ugly way

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+
+	import { setClient } from 'svelte-apollo';
+	import client from '$lib/graphql/client';
+
+	setClient(client);
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<h1 class="text-3xl font-bold underline">Hello Tailwind</h1>
+<script>
+	import { query } from 'svelte-apollo';
+	import { GET_MY_TODOS } from '$lib/graphql/queries';
+
+	const todos = query(GET_MY_TODOS);
+</script>
+
+{#if $todos.loading}
+	Loading...
+{:else if $todos.error}
+	<h1>Error: {$todos.error.message}</h1>
+{:else}
+	{#each $todos.data.todos as todo}
+		<pre>TODO: {todo.title}</pre>
+	{/each}
+{/if}
