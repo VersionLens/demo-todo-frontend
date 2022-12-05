d6c93ee753af506db63dea016f9a1934d5acf31a
Fredrik Stockman <fredrikstockman@gmail.com>
2022-12-05 17:09:11 +0100

    Create todo input and submit button

---
+++ src/lib/components/TodoInput.svelte
@@ -0,0 +1,16 @@
+<div class="mt-1 sm:col-span-2 sm:mt-0 flex flex-1 items-center">
+	<div class="mt-1 sm:col-span-2 sm:mt-0 flex flex-1">
+		<input
+			type="text"
+			name="todo"
+			id="todo"
+			placeholder="Write a new todo here"
+			class="flex flex-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+		/>
+	</div>
+	<button
+		type="button"
+		class="ml-5 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		>Add</button
+	>
+</div>
